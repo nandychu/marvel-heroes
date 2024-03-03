70b2d1a589f59a4714242570eece0dd9a0653e2c
nandychu <nandychu@gmail.com>
2024-03-03 12:54:29 +0100

    Display favorites in favsPage

--- src/pages/Favs.tsx
+++ src/pages/Favs.tsx
@@ -1,4 +1,15 @@
+import { useContext } from "react";
+import { MyContext } from "../context";
+import { HeroesGrid } from "../components/HeroesGrid/HeroesGrid";
+
 export const FavsPage: React.FC = () => {
-    return <p>Favs</p>;
-  };
-  
+  const { favHeroes } = useContext(MyContext);
+  return favHeroes && favHeroes.length > 0 ? (
+    <HeroesGrid heroes={favHeroes}></HeroesGrid>
+  ) : (
+    <div>
+      <h1 style={{ color: "black", marginBottom: 0 }}>No Favorites Heroes</h1>
+      <h3 style={{ color: "black" }}>Start adding some heroes to favs</h3>
+    </div>
+  );
+};
